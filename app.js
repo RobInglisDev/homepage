cd1afeef6b9b91798dce13e29aa8d548b4f12070
Rob Inglis <rob.inglis@global.ntt>
2019-08-27 22:43:55 +0200

    Adding body-parser

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+// SETUP LISTENER & BODYPARSER
 app.listen(port, function () {
     console.log("Server listening on port " + port);
 });
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// ROUTE FOR /
 app.get("/", function (req, res) {
-    // console.log(req);
-    res.send("<h1>Rob Inglis</h1>");
+    res.sendFile(__dirname + "/index.html")
+});
+app.post("/", function (req, res) {
+    var firstName = req.body.firstName;
+    res.send("Got It: " + firstName);
 })
+
+// ROUTE FOR /about
+app.get("/about", function (req, res) {
+    res.send("<h1>Rob Inglis - About</h1>");
+});
+
+// ROUTE FOR /contact
+app.get("/contact", function (req, res) {
+    res.send("<h1>Rob Inglis - Contact</h1>");
+});
 
 
